263d260a2d8d1bee348c12be08422fbc67229bfc
Fabiana Petrovick <fabypetrovick@gmail.com>
2021-08-07 21:29:32 -0300

    Initial Commit

---
+++ Gerencianet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gerencianet
+//
+//  Created by Gabriel Petrovick on 07/08/21.
+//  Copyright © 2021 Fabiana Petrovick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
