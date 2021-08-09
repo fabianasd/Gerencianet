0409a54d8202960c45418cb9c47576b262898806
Fabiana Petrovick <fabypetrovick@gmail.com>
2021-08-08 23:40:41 -0300

    feat: configuração do xib

--- Gerencianet/ViewController.swift
+++ Gerencianet/ViewController.swift
@@ -8,13 +8,122 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+    @IBOutlet weak var scrollView: UIScrollView!{
+            didSet{
+                scrollView.delegate = self
+            }
+        }
+        
+    @IBOutlet weak var pageControl: UIPageControl!
+        
+        var slides:[Slide] = [];
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            slides = createSlides()
+            setupSlideScrollView(slides: slides)
+            
+            pageControl.numberOfPages = slides.count
+            pageControl.currentPage = 0
+            view.bringSubviewToFront(pageControl)
+        }
+
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
+
+        
+        func createSlides() -> [Slide] {
+
+            let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+            slide1.labelTitle.text = "Soluções para descomplicar a gestão financeira, pensadas para você e seu negócio."
+            
+            let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+            slide2.labelTitle.text = "Soluções para descomplicar a gestão financeira, pensadas para você e seu negócio."
+            
+            let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+            slide3.labelTitle.text = "Soluções para descomplicar a gestão financeira, pensadas para você e seu negócio."
+            
+            let slide4:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+            slide4.labelTitle.text = "Soluções para descomplicar a gestão financeira, pensadas para você e seu negócio."
+            
+            let slide5:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+            slide5.labelTitle.text = "Soluções para descomplicar a gestão financeira, pensadas para você e seu negócio."
+            
+            return [slide1, slide2, slide3, slide4, slide5]
+        }
+        
+        
+        func setupSlideScrollView(slides : [Slide]) {
+            scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+            scrollView.isPagingEnabled = true
+            
+            for i in 0 ..< slides.count {
+                slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+                scrollView.addSubview(slides[i])
+            }
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+            pageControl.currentPage = Int(pageIndex)
+            
+            let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+            let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+            
+            // vertical
+            let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+            let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+            
+            let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+            let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+            
+            let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+            
+        }
+        
+        func scrollView(_ scrollView: UIScrollView, didScrollToPercentageOffset percentageHorizontalOffset: CGFloat) {
+            if(pageControl.currentPage == 0) {
+                
+                let pageUnselectedColor: UIColor = fade(fromRed: 255/255, fromGreen: 255/255, fromBlue: 255/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+                pageControl.pageIndicatorTintColor = pageUnselectedColor
+            
+                
+                let bgColor: UIColor = fade(fromRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1, toRed: 255/255, toGreen: 255/255, toBlue: 255/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+                slides[pageControl.currentPage].backgroundColor = bgColor
+                
+                let pageSelectedColor: UIColor = fade(fromRed: 81/255, fromGreen: 36/255, fromBlue: 152/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+                pageControl.currentPageIndicatorTintColor = pageSelectedColor
+            }
+        }
+        
+        func fade(fromRed: CGFloat,
+                  fromGreen: CGFloat,
+                  fromBlue: CGFloat,
+                  fromAlpha: CGFloat,
+                  toRed: CGFloat,
+                  toGreen: CGFloat,
+                  toBlue: CGFloat,
+                  toAlpha: CGFloat,
+                  withPercentage percentage: CGFloat) -> UIColor {
+            
+            let red: CGFloat = (toRed - fromRed) * percentage + fromRed
+            let green: CGFloat = (toGreen - fromGreen) * percentage + fromGreen
+            let blue: CGFloat = (toBlue - fromBlue) * percentage + fromBlue
+            let alpha: CGFloat = (toAlpha - fromAlpha) * percentage + fromAlpha
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        }
+        
+        override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+            setupSlideScrollView(slides: slides)
+        }
     }
 
 
-}
+
 
---
+++ Gerencianet/views/Slide.swift
@@ -0,0 +1,15 @@
+//
+//  Slide.swift
+//  Gerencianet
+//
+//  Created by Fabiana Petrovick on 08/08/21.
+//  Copyright © 2021 Fabiana Petrovick. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    
+    @IBOutlet weak var labelTitle: UITextView!
+    
+}
