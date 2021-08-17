90340cb5c6e3f504076b53f17c2274d6dfb8adc4
Fabiana Petrovick <fabypetrovick@gmail.com>
2021-08-17 20:09:38 -0300

    fix: alteração do slide

--- Gerencianet/views/Slide.swift
+++ Gerencianet/views/Slide.swift
@@ -11,6 +11,18 @@ import UIKit
 class Slide: UIView {
     
     @IBOutlet weak var labelTitle: UITextView!
+    @IBOutlet weak var constraintBottom: NSLayoutConstraint!
+    
+    
+    private func showCard() {
+        if let safeAreaHeight = UIApplication.shared.keyWindow?.safeAreaLayoutGuide.layoutFrame.size.height,
+            let bottomPadding = UIApplication.shared.keyWindow?.safeAreaInsets.bottom {
+            
+            // when card state is normal, its top distance to safe area is
+            // (safe area height + bottom inset) / 2.0
+            constraintBottom.constant = (safeAreaHeight + bottomPadding)/3.0
+        }
+        
+    }
     
 }
-
